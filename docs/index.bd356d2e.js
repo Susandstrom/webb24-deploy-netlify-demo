e6fb94c1ee09a5f4093270245921f28112895de5
Susandstrom <suthada.sandstrom@gritacademy.se>
2024-10-01 10:28:43 +0200

    edit

---
+++ docs/index.bd356d2e.js
@@ -0,0 +1,2 @@
+console.log("hej"),document.querySelector("h1"),h1.style.color="hotpink";
+//# sourceMappingURL=index.bd356d2e.js.map
--- src/modules/main.js
+++ src/modules/main.js
@@ -1,4 +1,4 @@
 console.log('hej');
 
-document.querySelector('h1');
+const h1 = document.querySelector('h1');
 h1.style.color = 'hotpink';
